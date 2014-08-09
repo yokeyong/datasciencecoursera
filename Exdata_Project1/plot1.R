583078f695e8f01235e1859d8538575dd52ffc85
ugoondu <yokeyong.wong@gmail.com>
2014-08-09 23:28:14 +0800

    Exploratory Data Analysis Project 1

---
+++ Exdata_Project1/plot1.R
@@ -0,0 +1,37 @@
+## Download data set directly from the internet. Files
+## are downloaded to temp directory of R.
+
+fileUrl = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+td = tempdir()
+tf = tempfile(tmpdir=td, fileext=".zip")
+
+## Method is set to "auto" under Windows. Data folder is
+## created under the working directory to allow easy access.
+
+download.file(fileUrl, tf, method="auto")
+unzip(tf, exdir=".", overwrite=TRUE)
+
+## Deletes the temporary files inside R.
+
+unlink(td)
+unlink(tf)
+
+data=read.table("household_power_consumption.txt", sep=";", 
+                stringsAsFactors=FALSE, skip=66637, 
+                nrow=2880)
+
+colnames(data) = c("Date", "Time", "Global_active_power", "Global_reactive_power", 
+                   "Voltage", "Global_intensity", "Sub_metering_1", 
+                   "Sub_metering_2", "Sub_metering_3")
+
+data$Global_active_power = as.numeric(data$Global_active_power)
+data$Date = as.Date(data$Date, format="%d/%m/%Y")
+
+## Plots a histogram for "plot1.png". The plot is saved to the
+## default working directory of your computer.
+
+hist(data$Global_active_power, col="red", main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)")
+
+dev.copy(png, "plot1.png")
+dev.off()
---
+++ Exdata_Project1/plot4.R
@@ -0,0 +1,46 @@
+## Continues from plot3.R. Code for downloading is removed.
+
+png(filename="plot4.png", type = "cairo")
+
+data=read.table("household_power_consumption.txt", sep=";", 
+                stringsAsFactors=FALSE, skip=66637, 
+                nrow=2880)
+
+colnames(data) = c("Date", "Time", "Global_active_power", "Global_reactive_power", 
+                   "Voltage", "Global_intensity", "Sub_metering_1", 
+                   "Sub_metering_2", "Sub_metering_3")
+
+data$Global_active_power = as.numeric(data$Global_active_power)
+times = strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S", tz="UTC")
+
+## Plots a combo graph for "plot4.png". The plot is saved to the
+## default working directory of your computer.
+
+## Setting environment for 4 plots together
+
+par(mfrow=c(2,2))
+
+## Function calls for Plot 1
+
+plot_1 = plot(x = times, y = data$Global_active_power, type="l", lwd = 0.5,
+         xlab = "", ylab = "Global Active Power")
+
+## Function calls for Plot 2
+
+plot_2 = plot(x = times, y = data$Voltage, type="l", lwd = 0.5,
+         xlab = "datetime", ylab = "Voltage")
+
+## Function calls for Plot 3
+
+plot_3 = c(plot(x = times, y = data$Sub_metering_1, type="s", xlab = "", ylab = "Energy sub metering"), points(x = times, data$Sub_metering_2, type="s", col = "red"),
+           points(x = times, data$Sub_metering_3, type="s", col = "blue"), 
+           legend("topright", lty=1, col = c("black", "red", "blue"), bty="n",
+                  legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")))
+  
+## Function calls for Plot 4
+
+plot_4 = plot(x = times, y = data$Global_reactive_power, type="l", lwd = 0.5, 
+              xlab = "datetime", ylab = "Global_reactive_power", ylim=c(0,0.5))
+
+
+dev.off()
